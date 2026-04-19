6e8a827ea8480b69c9bfa1055ba80a8e167a6fbf
Reuben Humphrey <reuben@rjh.nz>
2026-04-19 21:36:09 +1200

    clean up more filenames

--- indexer.py
+++ indexer.py
@@ -4,17 +4,11 @@ import json
 
 filesystem = dict()
 
-for year in Path("R:/Reuben/alumni-photos").iterdir():
-    path = "R:/Reuben/alumni-photos/" + year.name
+for year in Path("R:\\Reuben\\alumni-photos").iterdir():
+    path = "R:\\Reuben\\alumni-photos\\" + year.name
     filesystem[year.name] = glob.glob("*.*", root_dir=path)
 
-for key in filesystem:
-    print(filesystem[key])
-    print("\n")
-
 filesystemJSON = json.dumps(filesystem)
-
-print(filesystemJSON)
 
 with open("js/photoIndex.js", "w") as index:
     index.write("const fileStructure = ")
